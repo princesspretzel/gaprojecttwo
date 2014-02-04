43068abcb1001a34f9df16af6e7bd0408a531f27
zumbalogy <intrapostal@gmail.com>
2014-02-04 16:05:49 -0500

    ajax call for save

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -10,16 +10,24 @@
     }  
     $('body').on('keypress', function(e){
         if (e.which == 13) {
+            $.ajax({
+             method: 'post',
+             dataType: 'json',
+             url: '/save',
+             data: {city: $('#city').val(), genre: $('#genre').val()}
+            })
+         
+         
+         
             SC.get("/tracks", {q:""+$('#city').val()+"", genres:""+$('#genre').val()+"", limit: 10}, function(tracks){
-            songs = tracks;
-            console.log(songs);
-            for (i = 0; i < songs.length; i++) {
-                songs[i].stream_url = songs[i].stream_url+"?client_id=4c2a3b5840e0236549608f59c2cd7d07"
-                console.log(songs[i].stream_url)
-
-            }
-            audio.appendTo('body');
-            addSource(audio, songs[0].stream_url);
+                songs = tracks;
+                console.log(songs);
+                for (i = 0; i < songs.length; i++) {
+                    songs[i].stream_url = songs[i].stream_url+"?client_id=4c2a3b5840e0236549608f59c2cd7d07"
+                    console.log(songs[i].stream_url)
+                }
+                audio.appendTo('body');
+                addSource(audio, songs[0].stream_url);
             });
         };
     });
